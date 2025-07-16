d2e91319319381c05b98520841ea214287f586d7
Daniel Hernandez <d0h0dfl@cl.wal-mart.com>
2025-07-16 18:24:02 +0200

    add imageUrl field to menu static data

--- src/data/menus.ts
+++ src/data/menus.ts
@@ -3,12 +3,14 @@ import { MenuItem } from "@/interfaces/MenuItem";
 export const menus: Record<string, MenuItem[]> = {
   "tasty-burger": [
     {
+      imageUrl: "",
       id: "1", 
       name: "Hamburguesa Clásica",
       description: "Carne 100% res, lechuga, tomate y mayonesa.",
       price: 6500,
     },
     {
+      imageUrl: "",
       id: "2",
       name: "Papas Fritas",
       description: "Crocantes y doradas.",
@@ -17,12 +19,14 @@ export const menus: Record<string, MenuItem[]> = {
   ],
   "pizza-love": [
     {
+      imageUrl: "",
       id: "3",
       name: "Pizza Margarita",
       description: "Mozzarella, albahaca y salsa de tomate.",
       price: 7900,
     },
     {
+      imageUrl: "",
       id: "4",
       name: "Pizza Pepperoni",
       description: "Con extra pepperoni.",
@@ -31,12 +35,14 @@ export const menus: Record<string, MenuItem[]> = {
   ],
   "sushi-mania": [
     {
+      imageUrl: "",
       id: "5",
       name: "Roll California",
       description: "Kanikama, palta y pepino.",
       price: 7800,
     },
     {
+      imageUrl: "",
       id: "6",
       name: "Roll Tempura",
       description: "Salmón frito, queso crema.",
